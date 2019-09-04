ef8efec7bf528ebb0414bfc475a8cd13ae150074
FJJI <fjjimenez@miuandes.cl>
2019-09-04 19:33:18 -0300

    no entraba al server por falta de '{', arreglado

--- server.cpp
+++ server.cpp
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include "util.h"
+#include <cstdio>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,8 +24,7 @@ int main(int argc, char * argv[]) {
 	int sflag = 0;
 	int opt;
 	string s = "-s";
-	if (argc > 1){
-		cout <<argv[1];
+
 
 	// Procesar opciones de linea de comando
     	while ((opt = getopt (argc, argv, "s:")) != -1) {
@@ -44,10 +44,9 @@ int main(int argc, char * argv[]) {
 		cout<< "\nRuta De escucha acualizada, ahora sera: " << socket_path<<endl;
 		//paso ya el flag del -s
 		}
-
-	cout<<"SSS"<<endl;
-
+//conexion
 	if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+		cout<<"error de socket"<<endl;
 		perror("socket error");
 		exit(-1);
 		}
@@ -55,40 +54,46 @@ int main(int argc, char * argv[]) {
 	memset(&addr, 0, sizeof(addr));
 	addr.sun_family = AF_UNIX;
 	if (*socket_path == '\0') {
-	*addr.sun_path = '\0';
-	strncpy(addr.sun_path+1, socket_path+1, sizeof(addr.sun_path)-2);
-		} else {
+		*addr.sun_path = '\0';
+		strncpy(addr.sun_path+1, socket_path+1, sizeof(addr.sun_path)-2);
+	} else {
 		strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
 		unlink(socket_path);
 			}
 
 	if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+		cout<<"Error de bind"<<endl;
 		perror("bind error");
 		exit(-1);
 		}
 
 	if (listen(fd, 5) == -1) {
+		cout<<"Error de listen"<<endl;
 		perror("listen error");
 		exit(-1);
 		}
 
 	while (1) {
 		if ( (cl = accept(fd, NULL, NULL)) == -1) {
+			cout<<"Error"<<endl;
 			perror("accept error");
 			continue;
 			}
 
 		while ( (rc=read(cl,buf,sizeof(buf))) > 0) {
-		    	if(strcmp(buf,"quit")){
+			cout<<"s"<<strcmp(buf,"quit")<<endl;
+		    	if(strcmp(buf,"quit")==0){
 		    		return 0;
-		    	}
+				}
  			printf("read %u bytes: %.*s\n", rc, rc, buf);
 			}
 		if (rc == -1) {
+		cout<<"Error de lectura"<<endl;
       		perror("read");
       		exit(-1);
     			}
     		else if (rc == 0) {
+		cout<<"cierre"<<endl;
       		printf("EOF\n");
       		close(cl);
     			}
@@ -117,4 +122,4 @@ int main(int argc, char * argv[]) {
 	cout << db[1000].size << " " << (int) db[1000].data[0] << endl;
 	
 	return 0;
-}}
+}
